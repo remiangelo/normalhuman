3595b3e18fc4d5c82e0985c820f99af739260b40
remiangelo <justryeoi@gmail.com>
2025-02-27 01:59:03 +0700

    FINAL VERCEL CLERK

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -11,8 +11,15 @@ export const POST = async (req: Request) => {
     const imageUrl = data.image_url
     const id = data.id
 
-    await db.user.create({
-        data: {
+    await db.user.upsert({
+        where: {id: id},
+        update: {
+            emailAddress: emailAddress,
+            firstName: firstName,
+            lastName: lastName,
+            imageUrl: imageUrl,
+        },
+        create: {
             id: id,
             emailAddress: emailAddress,
             firstName: firstName,
