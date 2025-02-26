9cf21be748e3cdb4fbaf2523c3a0ed54c249076b
remiangelo <justryeoi@gmail.com>
2025-02-26 16:44:09 +0700

    test

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,15 @@
+// api/clerk/webhook
+
+import { db } from "@/server/db"
+
+export const POST = async (req: Request) => {
+    const {data} = await req.json()
+
+    const emailAddress = data.email_addresses[0].email_address
+    const firstName = data.first_name
+    const lastName = data.last_name
+    const imageUrl = data.image_url
+    const id = data.id
+
+    return new Response('Webhook received', {status: 200})
+}
---
+++ src/playground.ts
@@ -0,0 +1,10 @@
+import { db } from "./server/db";
+
+await db.user.create({
+    data: {
+        emailAddress: 'test@test.com',
+        firstName: 'Test',
+        lastName: 'Test',
+        }
+});
+
